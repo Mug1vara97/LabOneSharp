bbac67225b9c4abe3506c534d858ccc974f54787
Mug1vara97 <mugivarakun97@gmail.com>
2024-02-12 18:53:18 +0300

    5

---
+++ Bankomat/LabTwo.3/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+class SnyatSoSchetaException : Exception
+{
+    public SnyatSoSchetaException(string message) : base(message) { }
+}
+
+class OstatokNaScheteException : Exception
+{
+    public OstatokNaScheteException(string message) : base(message) { }
+}
+
+class Schet
+{
+    public int Номер { get; set; }
+    public int PINКод { get; set; }
+    private double _остаток;
+
+    public double Остаток
+    {
+        get { return _остаток; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new OstatokNaScheteException("Некорректное значение остатка на счете: " + value);
+            }
+            _остаток = value;
+        }
+    }
+
+    public virtual void СнятьСоСчета(double сумма)
+    {
+        try
+        {
+            if (сумма < 0 || сумма > Остаток)
+            {
+                throw new SnyatSoSchetaException("Некорректное значение суммы для снятия со счета: " + сумма);
+            }
+            Остаток -= сумма;
+            Console.WriteLine("Снятие выполнено успешно. Остаток на счете: " + Остаток);
+        }
+        catch (SnyatSoSchetaException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (OstatokNaScheteException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
+
+class ObichnySchet : Schet
+{
+    public override void СнятьСоСчета(double сумма)
+    {
+        base.СнятьСоСчета(сумма);
+    }
+}
+
+class LgotnySchet : Schet
+{
+    public override void СнятьСоСчета(double сумма)
+    {
+        base.СнятьСоСчета(сумма);
+    }
+}
+
+class Bankomat
+{
+    public string ИдентификационныйНомер { get; set; }
+    public string Адрес { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ObichnySchet obichnySchet = new ObichnySchet();
+        obichnySchet.Номер = 1234567890;
+        obichnySchet.PINКод = 1111;
+        obichnySchet.Остаток = 1000;
+
+        LgotnySchet lgotnySchet = new LgotnySchet();
+        lgotnySchet.Номер = 987654321;
+        lgotnySchet.PINКод = 2222;
+        lgotnySchet.Остаток = 2000;
+
+        Bankomat bankomat = new Bankomat();
+        bankomat.ИдентификационныйНомер = "BM001";
+        bankomat.Адрес = "ул. Пролетарская, 10";
+
+        Console.WriteLine("обычный сёчт:");
+        obichnySchet.СнятьСоСчета(500); // Снятие выполнено успешно. Остаток на счете: 500
+        obichnySchet.СнятьСоСчета(700); // Ошибка: Некорректное значение суммы для снятия со счета: 700
+        obichnySchet.СнятьСоСчета(-200); // Ошибка: Некорректное значение суммы для снятия со счета: -200
+        obichnySchet.СнятьСоСчета(2000); // Ошибка: Некорректное значение суммы для снятия со счета: 2000
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("лготный сёчт:");
+        lgotnySchet.СнятьСоСчета(500); // Ошибка: Некорректное значение суммы для снятия со счета: 1500
+        lgotnySchet.СнятьСоСчета(1500); // Ошибка: Некорректное значение суммы для снятия со счета: 1500
+    }
+}
---
+++ Enterprises/LabTwo.1/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+class Company
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+/// <summary>
+/// 
+/// </summary>
+class Department
+{
+    private string name;
+    private int numOfEmployees;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int NumOfEmployees
+    {
+        get { return numOfEmployees; }
+        set { numOfEmployees = value; }
+    }
+}
+
+class Employee
+{
+    private string fullName;
+    private string position;
+    private double salary;
+    private string departmentName;
+    private int departmentNumOfEmployees;
+    private string companyName;
+
+    public string FullName
+    {
+        get { return fullName; }
+        set { fullName = value; }
+    }
+
+    public string Position
+    {
+        get { return position; }
+        set { position = value; }
+    }
+
+    public double Salary
+    {
+        get { return salary; }
+        set { salary = value; }
+    }
+
+    public string DepartmentName
+    {
+        get { return departmentName; }
+        set { departmentName = value; }
+    }
+
+    public int DepartmentNumOfEmployees
+    {
+        get { return departmentNumOfEmployees; }
+        set { departmentNumOfEmployees = value; }
+    }
+
+    public string CompanyName
+    {
+        get { return companyName; }
+        set { companyName = value; }
+    }
+
+    public double CalculateSalary()
+    {
+        try
+        {
+            if (salary < 0)
+            {
+                throw new SalaryException("Невозможно создать сотрудника – указан отрицательный оклад:" + salary);
+            }
+
+            return salary;
+        }
+        catch (SalaryException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+
+        return 0;
+    }
+}
+
+class FullTimeEmployee : Employee
+{
+    private double bonus;
+
+    public double Bonus
+    {
+        get { return bonus; }
+        set { bonus = value; }
+    }
+
+    public double CalculateSalary()
+    {
+        try
+        {
+            if (bonus < 0)
+            {
+                throw new BonusException("Отрицательное значение премии.");
+            }
+
+            return base.CalculateSalary() + bonus;
+        }
+        catch (BonusException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+
+        return base.CalculateSalary();
+    }
+}
+
+class ContractEmployee : Employee
+{
+    public double CalculateSalary()
+    {
+        return base.CalculateSalary();
+    }
+}
+
+class SalaryException : Exception
+{
+    public SalaryException(string message) : base(message) { }
+}
+
+class BonusException : Exception
+{
+    public BonusException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            FullTimeEmployee employee1 = new FullTimeEmployee();
+            employee1.FullName = "Test";
+            employee1.Position = "zxc";
+            employee1.Salary = -2000;
+            employee1.Bonus = -500;
+            employee1.CompanyName = "ABC Company";
+            employee1.DepartmentName = "IT";
+            employee1.DepartmentNumOfEmployees = 10;
+            double salary1 = employee1.CalculateSalary();
+            Console.WriteLine(employee1.CompanyName + "\n" + employee1.DepartmentName + "\n" + employee1.FullName + "\n" + employee1.Position + "\n" + "Зарплата: " + salary1);
+
+            ContractEmployee employee2 = new ContractEmployee();
+            employee2.Salary = 1500;
+            employee2.CompanyName = "ABC Company";
+            employee2.DepartmentName = "HR";
+            employee2.DepartmentNumOfEmployees = 5;
+            double salary2 = employee2.CalculateSalary();
+            Console.WriteLine("Зарплата: " + salary2);
+        }
+        catch (SalaryException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (BonusException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
